51eff8694075c429c68ca269864a09a8a289821b
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-03 22:24:03 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,21 @@
+# aaa
+import java.io.*;
+import java.util.*;
+public class Even
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner (System.in);
+System.out.println("Enter the number");
+a=sc.nextInt();
+if(a%2==0)
+{
+System.out.println("Even number");
+}
+else
+{
+System.out.println("Odd number");
+}
+}
+}
